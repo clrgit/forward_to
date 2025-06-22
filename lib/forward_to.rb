aa67501ffb67f6d9008d701e05737e08cf5a782e
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2025-06-22 06:45:51 +0200

    * Added #attr_forward

--- lib/forward_to.rb
+++ lib/forward_to.rb
@@ -13,8 +13,8 @@ module ForwardTo
   #   class MyArray
   #     forward_to :@implementation, :empty?, :size, :[]
   #
-  #     def initialize()
-  #       @implementation = []
+  #     def initialize(a = [])
+  #       @implementation = a
   #     end
   #   end
   #
@@ -24,8 +24,7 @@ module ForwardTo
   #   a[0] = 1
   #   a[0] # -> 1
   #
-  # The target of #forward_to can be a method or a member (@variable) but has
-  # to be a symbol
+  # The target of #forward_to can be an attribute or a member variable
   #
   def forward_to(target, *methods)
     for method in Array(methods).flatten
@@ -38,6 +37,28 @@ module ForwardTo
           class_eval("def #{method}(*args, &block) #{target}&.#{method}(*args, &block) end")
       end
     end
+  end
+
+  # List #forward_to but also an attr reader method for the member object:
+  #
+  #   include ForwardTo
+  #
+  #   class MyArray
+  #     attr_forward :implementation, :empty?, :size, :[]
+  #
+  #     def initialize(a = [])
+  #       @implementation = a
+  #     end
+  #   end
+  #
+  #   a = MyArray.new([1, 2, 3])
+  #   a.implementation # -> [1, 2, 3]
+  #
+  # The target of #attr_reader should be an attribute
+  #
+  def attr_forward(target, *methods)
+    class_eval("attr_reader :#{target}")
+    forward_to(target, *methods)
   end
 end
 
