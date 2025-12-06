460e4f2f9e491af78724d5d7aba952138aceddac
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2025-12-06 02:45:51 +0100

    Added forward_to_class

--- lib/forward_to.rb
+++ lib/forward_to.rb
@@ -3,7 +3,7 @@
 require_relative "forward_to/version"
 
 module ForwardTo
-  # Forward methods to member object. The arguments should be strings or symbols
+  # Forward methods to member object, arguments can be strings or symbols
   #
   # Forward to takes the first argument and creates methods for the rest of the
   # arguments that forward their call to the first argument. Example
@@ -23,23 +23,35 @@ module ForwardTo
   #   a.empty? # -> true
   #   a[0] = 1
   #   a[0] # -> 1
+  #   a.implementation # Error - see attr_forward
   #
   # The target of #forward_to can be an attribute or a member variable
   #
-  def forward_to(target, *methods)
-    for method in Array(methods).flatten
-      case method
-        when /\[\]=/
-          class_eval("def #{method}(*args) #{target}&.#{method}(*args) end")
-        when /=$/
-          class_eval("def #{method}(args) #{target}&.#{method}(args) end")
-        else
-          class_eval("def #{method}(*args, &block) #{target}&.#{method}(*args, &block) end")
-      end
-    end
-  end
+  def forward_to(target, *methods) = impl_forward_to(target, methods, class_method: false)
 
-  # Like #forward_to but also an attr reader method for the member object:
+  # :call-seq:
+  #   forward_to_class(class, *methods)
+  #   forward_to_class(class-method, *methods)
+  #
+  # Create class methods that forwards to class or to the given class method:
+  #
+  #   module Mod
+  #     def self.module_method() = "other"
+  #   end
+  #
+  #   class Klass
+  #     def self.class_method() = "string"
+  #     forward_to_class :class_method, :size
+  #     forward_to_class Mod, :module_method
+  #   end
+  #
+  #   Klass.size # -> 5
+  #   Klass.module_method # -> "other"
+  #
+  def forward_to_class(target, *methods) = impl_forward_to(target.to_s, methods, class_method: true)
+
+
+  # Like #forward_to but also add an attr reader method for the member object:
   #
   #   include ForwardTo
   #
@@ -59,6 +71,21 @@ module ForwardTo
   def attr_forward(target, *methods)
     class_eval("attr_reader :#{target}")
     forward_to(target, *methods)
+  end
+
+private
+  def impl_forward_to(target, *methods, class_method: false)
+    qual = class_method ? "self." : ""
+    for method in Array(methods).flatten
+      case method
+        when /\[\]=/
+          class_eval("def #{qual}#{method}(*args) #{target}&.#{method}(*args) end")
+        when /=$/
+          class_eval("def #{qual}#{method}(args) #{target}&.#{method}(args) end")
+        else
+          class_eval("def #{qual}#{method}(*args, &block) #{target}&.#{method}(*args, &block) end")
+      end
+    end
   end
 end
 
--- spec/forward_to_spec.rb
+++ spec/forward_to_spec.rb
@@ -8,7 +8,7 @@ describe "ForwardTo" do
 
   describe "#forward_to" do
     # The target class
-    refklass = Class.new do
+    target_klass = Class.new do
       def var() @value end
       def var=(value) @value = value end
       def initialize()
@@ -20,34 +20,65 @@ describe "ForwardTo" do
     klass = Class.new do
       forward_to :@ref, :var, :var=
       forward_to :@array, :size, :+, :[], :[]=
-      def initialize(refklass)
-        @ref = refklass
+      def initialize(target_klass)
+        @ref = target_klass
         @array = [1, 2, 3]
       end
     end
 
-    let(:a) { klass.new(refklass.new) }
+    let(:a) { klass.new(target_klass.new) }
 
-    it "forwards the given methods to the forwarding object" do
+    it "forwards the given methods to the target object" do
       expect(a.size).to eq 3
       expect(a[1]).to eq 2
       expect(a + [4, 5]).to eq [1, 2, 3, 4, 5]
     end
+
     it "handles #[]=" do
       expect(a[0] = 2).to eq 2
       expect(a[0] += 10).to eq 12
       expect(a[0]).to eq 12
     end
+
     it "handles #=" do
       expect(a.var).to eq "value"
       expect { a.var = "VALUE" }.not_to raise_error
       expect(a.var).to eq "VALUE"
     end
+
     it "handles block arguments"
   end
 
+  describe "#forward_to_class" do
+    before do
+      stub_const 'TargetKlass', Class.new
+      TargetKlass.class_eval do
+        def self.class_method() "TargetKlass::class_method"  end
+      end
+
+      stub_const 'Klass', Class.new
+      Klass.class_eval do
+        forward_to_class TargetKlass, :class_method
+        forward_to_class :class_object, :keys, :values
+        def self.class_object() { key: "val" } end
+      end
+    end
+
+    context "when target is a class" do
+      it "forwards the given class methods to the target class" do
+        expect(Klass.class_method).to eq "TargetKlass::class_method"
+      end
+    end
+
+    context "when target is a method symbol" do
+      it "forwards the given class methods to the class method" do
+        expect(Klass.keys).to eq [:key]
+      end
+    end
+  end
+
   describe "#attr_forward" do
-    refklass = Class.new do
+    target_klass = Class.new do
       def var() @value end
       def var=(value) @value = value end
       def initialize()
@@ -58,13 +89,13 @@ describe "ForwardTo" do
     klass = Class.new do
       attr_forward :ref, :var, :var=
       attr_forward :array, :size, :+, :[], :[]=
-      def initialize(refklass)
-        @ref = refklass
+      def initialize(target_klass)
+        @ref = target_klass
         @array = [1, 2, 3]
       end
     end
 
-    let(:a) { klass.new(refklass.new) }
+    let(:a) { klass.new(target_klass.new) }
 
     it "create an attr_reader for the forwarding object" do
       expect(a.ref.var).to eq "value"
